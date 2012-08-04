f338b6b24d86de316be9dcfaac42573b7022f8fd
Kyoung-Rok Jang <astray0924@gmail.com>
2012-08-04 17:34:46 +0900

    pos_tagging (sentence)

--- document.py
+++ document.py
@@ -3,27 +3,26 @@ from nltk.corpus import brown, movie_reviews
 import nltk
 import random
 
-def pos_features(word):
-    features = {}
-    for suffix in common_suffixes:
-        features['endswith(%s)' % suffix] = word.lower().endswith(suffix)
+def pos_features(sentence, i):
+    features = {"suffix(1)": sentence[i][-1:],
+                "suffix(2)": sentence[i][-2:],
+                "suffix(3)": sentence[i][-3:]}
+    if i == 0:
+        features["prev-word"] = "<START>"
+    else:
+        features["prev-word"] = sentence[i - 1]
     return features
 
 if __name__ == '__main__':
-    # 사용할 feature 생성
-    suffix_fdist = nltk.FreqDist()
-    for word in brown.words():
-        word = word.lower()
-        suffix_fdist.inc(word[-1:])
-        suffix_fdist.inc(word[-2:])
-        suffix_fdist.inc(word[-3:])
-    common_suffixes = suffix_fdist.keys()[:100]
-
-    # feature를 이용해서 DecisionTreeClassifier를 훈련함
-    tagged_words = brown.tagged_words(categories='news')
-    featuresets = [(pos_features(n), g) for (n, g) in tagged_words]
+    tagged_sents = brown.tagged_sents(categories='news')
+    featuresets = []
+    for tagged_sent in tagged_sents:
+        untagged_sent = nltk.tag.untag(tagged_sent)
+        for i, (word, tag) in enumerate(tagged_sent):
+            featuresets.append((pos_features(untagged_sent, i), tag))
+            
     size = int(len(featuresets) * 0.1)
     train_set, test_set = featuresets[size:], featuresets[:size]
+    classifier = nltk.NaiveBayesClassifier.train(train_set)
     
-    classifier = nltk.DecisionTreeClassifier.train(train_set)
     print nltk.classify.accuracy(classifier, test_set)
