441a20452976b82f6abbad445708d6d6694f9206
Kyoung-Rok Jang <astray0924@gmail.com>
2012-08-04 17:49:45 +0900

    ConsecutivePosTagger

--- document.py
+++ document.py
@@ -1,28 +1,45 @@
 # -*- coding: utf-8 -*-
 from nltk.corpus import brown, movie_reviews
 import nltk
-import random
 
-def pos_features(sentence, i):
+def pos_features(sentence, i, history):
     features = {"suffix(1)": sentence[i][-1:],
                 "suffix(2)": sentence[i][-2:],
                 "suffix(3)": sentence[i][-3:]}
+    
     if i == 0:
-        features["prev-word"] = "<START>"
+        features["prev_word"] = "<START>"
+        features["prev_tag"] = "<START>"
     else:
-        features["prev-word"] = sentence[i - 1]
+        features["prev_word"] = sentence[i - 1]
+        features["prev_tag"] = history[i - 1]
+        
     return features
+
+class ConsecutivePosTagger(nltk.TaggerI):
+    def __init__(self, train_sents):
+        train_set = []
+        for tagged_sent in train_sents:
+            untagged_sent = nltk.tag.untag(tagged_sent)
+            history = []
+            for i, (word, tag) in enumerate(tagged_sent):
+                featureset = pos_features(untagged_sent, i, history)
+                train_set.append( (featureset, tag) )
+                history.append(tag)
+        self.classifier = nltk.NaiveBayesClassifier.train(train_set)
+        
+    def tag(self, sentence):
+        history = []
+        for i, word in enumerate(sentence):
+            featureset = pos_features(sentence,i, history)
+            tag = self.classifier.classify(featureset)
+            history.append(tag)
+        return zip(sentence, history)
 
 if __name__ == '__main__':
     tagged_sents = brown.tagged_sents(categories='news')
-    featuresets = []
-    for tagged_sent in tagged_sents:
-        untagged_sent = nltk.tag.untag(tagged_sent)
-        for i, (word, tag) in enumerate(tagged_sent):
-            featuresets.append((pos_features(untagged_sent, i), tag))
-            
-    size = int(len(featuresets) * 0.1)
-    train_set, test_set = featuresets[size:], featuresets[:size]
-    classifier = nltk.NaiveBayesClassifier.train(train_set)
+    size = int(len(tagged_sents) * 0.1)
+    train_sents, test_sents = tagged_sents[size:], tagged_sents[:size]
+    tagger = ConsecutivePosTagger(train_sents)
+    print tagger.evaluate(test_sents)
     
-    print nltk.classify.accuracy(classifier, test_set)
